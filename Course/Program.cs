fa852b51b06d93c433fde25515d83447aaabd7e3
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-06 18:44:41 -0300

    Client class created and usage in main class

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Testing client class
+
+using Course.Entities;
+
+Console.WriteLine("Enter client data: ");
+Console.Write("Name: ");
+string name = Console.ReadLine();
+
+Console.Write("Email: ");
+string email = Console.ReadLine();
+
+Console.Write("Birth date (DD/MM/YYYY): ");
+DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+Client client = new Client(name, email, birthDate);
+
+Console.WriteLine();
+Console.WriteLine("Client: " + client);
+
+
+
