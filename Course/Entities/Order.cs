282c78eec45d6cc9d1d903d10ae289b9a55feeaa
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-06 20:18:09 -0300

    Order class, OrderStatus enum created and run program

---
+++ Course/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using Course.Entities.Enums;
+using System.Text;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; private set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+
+        public Order()
+        {
+            Moment = DateTime.Now;
+        }
+
+        public Order(OrderStatus status, Client client) : this()
+        {
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Order moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}")
+              .AppendLine($"Status: {Status.ToString()}")
+              .AppendLine($"Client: {Client}")
+              .AppendLine($"Order items: ");
+
+            foreach (OrderItem item in Items)
+            {
+                sb.Append(item.ToString());
+            }
+
+            sb.AppendLine($"Total price: {Total().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -1,19 +1,54 @@
 ﻿using Course.Entities;
+using Course.Entities.Enums;
 using System.Globalization;
 
-// Testing product class
+// Testing all
 
-Console.Write("Product name: ");
-string productName = Console.ReadLine();
-Console.Write("Product price: ");
-double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Enter client data: ");
+Console.Write("Name: ");
+string name = Console.ReadLine();
 
-Product product = new Product(productName, price);
+Console.Write("Email: ");
+string email = Console.ReadLine();
+
+Console.Write("Birth date (DD/MM/YYYY): ");
+DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+Client client = new Client(name, email, birthDate);
+
+Console.WriteLine("Enter order data: ");
+Console.Write("Status: ");
+OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+Console.Write("How many items to this order? ");
+int n = int.Parse(Console.ReadLine());
+
+Order order = new Order(status, client);
+
+OrderItem orderItem;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Enter #{i} data: ");
+    Console.Write("Product name: ");
+    string productName = Console.ReadLine();
+
+    Console.Write("Product price: ");
+    double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Product product = new Product(productName, productPrice);
+
+    Console.Write("Quantity: ");
+    int quantity = int.Parse(Console.ReadLine());
+
+    orderItem = new OrderItem(product, quantity);
+    order.AddItem(orderItem);
+}
 
 Console.WriteLine();
-Console.Write("Product: ");
-Console.WriteLine(product.Name + ", $ " + product.Price.ToString("F2", CultureInfo.InvariantCulture));
-
+Console.WriteLine("ORDER SUMMARY: ");
+Console.WriteLine(order);
 
 
 
