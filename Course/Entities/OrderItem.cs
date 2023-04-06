3d851e98c76c6786e2bf64f2650df59a6e59ffb7
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-06 19:30:11 -0300

    OrderItem class created

---
+++ Course/Entities/OrderItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Course.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(Product product, int quantity)
+        {
+            Product = product;
+            Price = Product.Price;
+            Quantity = quantity;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Product.Name}, ${Price.ToString("F2", CultureInfo.InvariantCulture)}, Quantity: {Quantity}, SubTotal: {SubTotal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+    }
+}
