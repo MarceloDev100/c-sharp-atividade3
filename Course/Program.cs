bce8c83c3625415f1c5bf29fe0d73a0783ba4253
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-06 19:10:05 -0300

    Product class created and usage in main class

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,21 +1,20 @@
-﻿// Testing client class
+﻿using Course.Entities;
+using System.Globalization;
 
-using Course.Entities;
+// Testing product class
 
-Console.WriteLine("Enter client data: ");
-Console.Write("Name: ");
-string name = Console.ReadLine();
+Console.Write("Product name: ");
+string productName = Console.ReadLine();
+Console.Write("Product price: ");
+double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-Console.Write("Email: ");
-string email = Console.ReadLine();
-
-Console.Write("Birth date (DD/MM/YYYY): ");
-DateTime birthDate = DateTime.Parse(Console.ReadLine());
-
-Client client = new Client(name, email, birthDate);
+Product product = new Product(productName, price);
 
 Console.WriteLine();
-Console.WriteLine("Client: " + client);
+Console.Write("Product: ");
+Console.WriteLine(product.Name + ", $ " + product.Price.ToString("F2", CultureInfo.InvariantCulture));
+
+
 
 
 
